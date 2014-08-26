e425ce4c797e33f8f6377ebdc3bcda8121d686aa
Bryant Pham <bryantphamm@gmail.com>
2014-08-25 22:08:11 -0500

    Added GPS Location updates

--- app/src/main/java/com/pokebro/Activity/MapsActivity.java
+++ app/src/main/java/com/pokebro/Activity/MapsActivity.java
@@ -1,8 +1,11 @@
 package com.pokebro.Activity;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -13,10 +16,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.pokebro.R;
 
-public class MapsActivity extends FragmentActivity  {
+public class MapsActivity extends FragmentActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private final String ACTIVITY_TAG = "MapActivity";
+    private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +32,31 @@ public class MapsActivity extends FragmentActivity  {
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
 
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                                    .addApi(LocationServices.API)
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     /**
@@ -70,5 +95,30 @@ public class MapsActivity extends FragmentActivity  {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(ACTIVITY_TAG, "GoogleApiClient Connected");
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5000); // Update location every 5 seconds
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(ACTIVITY_TAG, "GoogleApiClient Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(ACTIVITY_TAG, "GoogleApiClient Connection Failed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(ACTIVITY_TAG, "Location received: " + location.toString());
     }
 }
