8029e37c01731a97f37b20cfcf00f920b082db5a
Bryant Pham <bryantphamm@gmail.com>
2014-10-04 21:18:30 -0500

    Added decrementCounter method to Monster class

--- app/src/main/java/com/pokebro/Model/Monster.java
+++ app/src/main/java/com/pokebro/Model/Monster.java
@@ -7,6 +7,11 @@ public class Monster {
     private String name;
     private int image;
 
+    public Monster(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+
     public String getName() {
         return name;
     }
