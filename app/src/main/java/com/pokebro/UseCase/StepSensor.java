bf4a00a67d0e0e22ee31d63f8aa846fe2cb8479b
Bryant Pham <bryantphamm@gmail.com>
2014-10-05 23:03:51 -0500

    Changed StepSensor class to call GameEngine

--- app/src/main/java/com/pokebro/UseCase/StepSensor.java
+++ app/src/main/java/com/pokebro/UseCase/StepSensor.java
@@ -1,18 +1,12 @@
 package com.pokebro.UseCase;
 
 import android.content.Context;
-import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.pokebro.Activity.PokemonActivity;
-import com.pokebro.Model.RandomEncounter;
-
-import java.util.Random;
+import com.pokebro.GameEngine.GameEngine;
 
 /**
  * Created by Bryant on 9/17/2014.
@@ -21,12 +15,13 @@ public class StepSensor implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor stepSensor;
     private Context context;
-    private RandomEncounterManager randomEncounterManager;
+    private GameEngine gameEngine;
 
-    public StepSensor(Context context, SensorManager sensorManager, RandomEncounterManager randomEncounterManager) {
+
+    public StepSensor(Context context, SensorManager sensorManager, GameEngine gameEngine) {
         this.context = context;
-        this.randomEncounterManager = randomEncounterManager;
         this.sensorManager = sensorManager;
+        this.gameEngine = gameEngine;
         stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
     }
 
@@ -36,18 +31,12 @@ public class StepSensor implements SensorEventListener {
 
     public void startSensor() {
         sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        randomEncounterManager.resetCounter();
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        Toast.makeText(context, "Step Counter: " + randomEncounterManager.getCounter(), Toast.LENGTH_SHORT).show();
-        if(randomEncounterManager.encounterMonster()) {
-            stopSensor();
-            Intent pokemonActivityIntent = new Intent(context, PokemonActivity.class);
-            pokemonActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(pokemonActivityIntent);
-        }
+        //Toast.makeText(context, "Step Counter: " + randomEncounterManager.getCounter(), Toast.LENGTH_SHORT).show();
+        gameEngine.stepSensed();
     }
 
     @Override
