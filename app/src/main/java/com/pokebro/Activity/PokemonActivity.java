b3466edb72bc3c9ee1bded9aee8ff5b72ba50ad6
Bryant Pham <bryantphamm@gmail.com>
2014-10-19 13:49:45 -0500

    Added catch button and pokemon name under image

--- app/src/main/java/com/pokebro/Activity/PokemonActivity.java
+++ app/src/main/java/com/pokebro/Activity/PokemonActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.pokebro.Adapter.MonsterParcelable;
 import com.pokebro.Model.Monster;
@@ -16,7 +17,7 @@ import java.util.Random;
 
 public class PokemonActivity extends Activity {
     private ImageView pokemonView;
-    private RandomPokemonFactory pokemonFactory;
+    private TextView pokemonNameView;
     private Monster pokemon;
 
     @Override
@@ -30,6 +31,8 @@ public class PokemonActivity extends Activity {
 
         pokemonView = (ImageView) findViewById(R.id.pokemonView);
         pokemonView.setImageBitmap(resizedDrawable);
+        pokemonNameView = (TextView) findViewById(R.id.pokemonName);
+        pokemonNameView.setText(pokemon.getName());
     }
 
     public void backToMapActivity(View view) {
