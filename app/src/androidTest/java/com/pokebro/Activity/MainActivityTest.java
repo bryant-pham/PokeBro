43ac4118a0d4ae74127de3e3f5e9eaeed126f66a
Bryant Pham <bryantphamm@gmail.com>
2014-12-29 17:30:18 -0600

    Added many ui tests to MainActivityTest

--- app/src/androidTest/java/com/pokebro/Activity/MainActivityTest.java
+++ app/src/androidTest/java/com/pokebro/Activity/MainActivityTest.java
@@ -2,6 +2,7 @@ package com.pokebro.Activity;
 
 import android.support.v4.widget.DrawerLayout;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 
@@ -15,10 +16,14 @@ import static android.support.test.espresso.contrib.DrawerActions.closeDrawer;
 import static android.support.test.espresso.contrib.DrawerActions.openDrawer;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.not;
+
 /**
  * Created by BRYANT on 12/27/2014.
  */
+
+@LargeTest
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private MainActivity mainActivity;
@@ -56,4 +61,51 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         onView(withId(R.id.drawer_layout)).check(matches(isOpen()));
         onView(withId(R.id.left_drawer)).check(matches(isDisplayed()));
     }
+
+    public void testNavDrawerShouldClose() {
+        openDrawer(R.id.drawer_layout);
+        closeDrawer(R.id.drawer_layout);
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
+        onView(withId(R.id.left_drawer)).check(matches(not(isDisplayed())));
+    }
+
+    public void testNavDrawerShouldHaveDrawerItems() {
+        openDrawer(R.id.drawer_layout);
+        onView(allOf(withId(R.id.nav_drawer_item_text), hasSibling(withText("Queue List")))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.nav_drawer_item_text), hasSibling(withText("Caught List")))).check(matches(isDisplayed()));
+    }
+
+    public void testActivityShouldStartWithQueueFragment() {
+        onView(withId(R.id.monster_queue_fragment)).check(matches(isDisplayed()));
+    }
+
+    public void testClickOnQueueListNavItemShouldOpenQueueFragment() {
+        // Open nav drawer
+        openDrawer(R.id.drawer_layout);
+
+        // Click on drawer item
+        onView(allOf(withId(R.id.nav_drawer_item_text), hasSibling(withText("Queue List")))).perform(click());
+
+        // Assert caught list fragment is displayed
+        onView(withId(R.id.monster_queue_fragment)).check(matches(isDisplayed()));
+
+        // Assert nav drawer is closed afterwards
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
+    }
+
+    public void testClickOnCaughtListNavItemShouldOpenCaughtListFragment() {
+        // Open nav drawer
+        openDrawer(R.id.drawer_layout);
+
+        // Click on drawer item
+        onView(allOf(withId(R.id.nav_drawer_item_text), hasSibling(withText("Caught List")))).perform(click());
+
+        // Assert caught list fragment is displayed
+        onView(withId(R.id.caught_monster_fragment)).check(matches(isDisplayed()));
+
+        // Assert nav drawer is closed afterwards
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
+    }
+
+
 }
